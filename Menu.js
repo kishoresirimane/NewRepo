2b1edc092d23df68816caf566e5320987831ff5f
kishoresirimane <53344060+kishoresirimane@users.noreply.github.com>
2021-05-15 13:26:18 +0530

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,23 @@
+   function Menu() {
+  return (
+        <div className="App">
+            <p>
+      <a align="left">REACT LOGO</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a align="center" >REACT TUTORIAL</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hello UsessionStorage.getItem("userName");
+        
+        <br></br>
+        <a href="./Home">  HOME</a>&nbsp;
+        <a href="./SetUp"> SET UP</a>&nbsp;
+        <a href="./Jsx"> JSX</a>&nbsp;
+        <a href="./ApiCalls"> API CALLS</a>&nbsp;
+        <a href="./AppPhases"> APP PHASES</a>
+       </p>
+         </div>
+    );
+  }
+  
+  export default Menu;
